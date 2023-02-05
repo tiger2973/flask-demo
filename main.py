dacca705d7ffc7b751ef1dcca5bc3ddc4be7b55a
tiger2973 <124491753+tiger2973@users.noreply.github.com>
2023-02-05 17:22:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from flask import *
 
-def createapp(testing:bool = True):
-      app = Flask(__name__)  
-      
-      @app.route('/')  
-      def index():  
-            return render_template('index.html')  
-      return app
+app = Flask(__name__)  
+ 
+@app.route('/')  
+def index():  
+      return render_template('index.html')  
+if __name__ == '__main__':  
+   app.run(debug = True)
